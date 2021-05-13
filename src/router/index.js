c783982e6f4456efc98e558046cf0b08e66613bf
ManuelFabijancic1 <75333397+ManuelFabijancic1@users.noreply.github.com>
2021-05-13 18:50:03 +0200

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,46 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/workoutbuilder',
+    name: 'workoutbuilder',
+    component: () => import(/* webpackChunkName: "workoutbuilder" */  '../views/workoutbuilder.vue')
+  },
+  {
+    path: '/sleepcalculator',
+    name: 'sleepcalculator',
+    component: () => import(/* webpackChunkName: "sleepcalculator" */  '../views/sleepcalculator.vue')
+  },
+  {
+    path: '/workoutmenager',
+    name: 'workoutmenager',
+    component: () => import(/* webpackChunkName: "workoutmenager" */  '../views/workoutmenager.vue')
+  },
+  {
+    path: '/calorieintake',
+    name: 'calorieintake',
+    component: () => import(/* webpackChunkName: "calorieintake" */  '../views/CalorieIntakeCalculator.vue')
+  },
+  {
+    path: '/workoutresult',
+    name: 'workoutresult',
+    component: () => import(/* webpackChunkName: "workoutresult" */  '../views/workoutresult.vue')
+  },
+  {
+    path: '/bedtime',
+    name: 'bedtime',
+    component: () => import(/* webpackChunkName: "bedtime" */  '../views/bedtime.vue')
+  },
+  {
+    path: '/wakeuptime',
+    name: 'wakeuptime',
+    component: () => import(/* webpackChunkName: "wakeuptime" */  '../views/wakeuptime.vue')
+  },
+  {
+    path: '/workouttracker',
+    name: 'workouttracker',
+    component: () => import(/* webpackChunkName: "workouttracker" */  '../views/workouttracker.vue')
   }
 ]
 
